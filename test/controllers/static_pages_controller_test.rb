d11047680b67e6dffc51f5e9a753f1d9ae4ed1d5
Tiago Pereira <tiagohcp@gmail.com>
2020-08-28 14:11:37 -0300

    Paginas Estaticas

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get static_pages_index_url
+    assert_response :success
+  end
+
+  test "should get sobre" do
+    get static_pages_sobre_url
+    assert_response :success
+  end
+
+  test "should get contato" do
+    get static_pages_contato_url
+    assert_response :success
+  end
 end
