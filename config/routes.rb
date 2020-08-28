aea578d73c811727c38440a59f735a8829c47411
Tiago Pereira <tiagohcp@gmail.com>
2020-08-28 18:19:17 -0300

    Final

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-  root to: "static_pages#index"
-
+  
+  root to: "static_pages#index"  
   get 'sobre', to: 'static_pages#sobre'
   get 'contato', to: 'static_pages#contato'
 
+  get 'cadastro', to: 'users#new'
+  get 'entrar', to: 'sessions#new'
+  post 'entrar', to: 'sessions#create'
+  delete 'sair', to: 'sessions#destroy'
+  
+  resources :contacts
+  resources :users, only: [:new, :create, :show, :edit, :update]
+  
 end
